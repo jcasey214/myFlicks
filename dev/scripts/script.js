c18781fdd0e33fe5368a424bc02521edba221249
Jonathan Casey <jon_casey@me.com>
2015-11-16 16:00:43 -0700

    fix async calls

--- dev/scripts/script.js
+++ dev/scripts/script.js
@@ -4,31 +4,32 @@ $(function(){
   function successCB(data) {
     var result = JSON.parse(data);
     console.log(result);
-    // $div.append(result.results[0].original_title, result.results[0].overview + '\n');
-    //
-    //  $div.append('<img src="http://image.tmdb.org/t/p/w185' + result.results[0].poster_path + '">');
-    //  netflixStatus(result.results[0]);
-    var source = $('#result-template').html();
-    var template = Handlebars.compile(source);
-    var context = result.results[0];
-    context.poster_path = "http://image.tmdb.org/t/p/w185" + context.poster_path;
-    getCast(context).done(function(starring){
-      context.starring = starring;
-      getDirector(context).done(function(director){
-        context.director = director;
-        getTrailer(context).done(function(trailerLink){
-          context.trailer = trailerLink;
-          console.log(context);
-          var html = template(context);
-          $div.append(html);
-        });
-      });
+    for (var i = 0; i < 1; i++) {
+      var context = result.results[i];
+      context.poster_path = "http://image.tmdb.org/t/p/w185" + context.poster_path;
+      getDetailsForMovie(context);
+    }
+}
 
-    }).fail(function(error){
-      console.log(error);
+function getDetailsForMovie(movie) {
+  var source = $('#result-template').html();
+  var template = Handlebars.compile(source);
+  getCast(movie).done(function(movie){
+    getDirector(movie).done(function(movie){
+      getTrailer(movie).done(function(movie){
+        netflixStatus(movie).done(function(movie){
+        console.log(movie);
+        var html = template(movie);
+        $div.append(html);
+      });
+      });
     });
-  }
-//funtion to execute if api call to tmdb fails
+  }).fail(function(){
+    console.log(error);
+  });
+}
+
+//function to execute if api call to tmdb fails
   function errorCB(data) {
     console.log("Error callback: " + data);
     }
@@ -46,35 +47,44 @@ $(function(){
     theMovieDb.search.getMovie({"query": query}, successCB, errorCB);
   });
 //check availability of title on netflix using netflix roulette api
-  function netflixStatus(obj){
-      var year = obj.release_date.split('-')[0];
-      var title = obj.original_title;
-      $.get('https://netflixroulette.net/api/api.php?title=' + title + '&year=' + year)
-      .done(function(data){
-        console.log(data);
-      })
-      .fail(function(){
-        console.log('That title is not currently available');
-      });
+  function nnnetflixStatus(obj){
+    var deferred = $.Deferred();
+    var year = obj.release_date.split('-')[0];
+    var title = obj.original_title;
+    $.get('https://netflixroulette.net/api/api.php?title=' + title + '&year=' + year)
+    .done(function(data){
+      console.log(data);
+      obj.netflix = true;
+    })
+    .fail(function(){
+      console.log('That title is not currently available');
+      obj.netflix = false;
+    });
   }
 
-  function getCast(obj){
+
+  function getCast(movie){
     var deferred = jQuery.Deferred();
 
-    theMovieDb.movies.getCredits({"id": obj.id }, function(data){
+    theMovieDb.movies.getCredits({"id": movie.id },
+     function(data){
       var result = JSON.parse(data);
       console.log(result);
       // obj.director = result.crew[0].name;
       var cast = result.cast;
       var starring = 'Cast: ';
-      for (var i = 0; i < 5; i ++){
+      for (var i = 0; i < cast.length; i ++){
         starring += cast[i].name;
         if(i < 4){
           starring += ', ';
         }
+        if (i === 5){
+          break;
+        }
       }
       console.log(starring);
-      deferred.resolve(starring);
+      movie.starring = starring;
+      deferred.resolve(movie);
     }, function(error){
       deferred.reject(error);
     });
@@ -97,7 +107,8 @@ $(function(){
       }
        // get the director out of the result
       console.log(director);
-      deferred.resolve(director);
+      movie.director = director;
+      deferred.resolve(movie);
     }, function(error){
       deferred.reject(error);
     });
@@ -111,13 +122,34 @@ $(function(){
     theMovieDb.movies.getTrailers({"id": movie.id }, function(data){
       var result = JSON.parse(data);
       console.log(result);
-      // obj.director = result.crew[0].name;
-      var trailerLink = 'https: //www.youtube.com/watch?v=' + result.youtube[0].source;// get the director out of the result
-      console.log(trailerLink);
-      deferred.resolve(trailerLink);
+      if(result.youtube.length > 0) {
+        // obj.director = result.crew[0].name;
+        var trailerLink = 'https: //www.youtube.com/watch?v=' + result.youtube[0].source;// get the director out of the result
+        console.log(trailerLink);
+        movie.trailerLink = trailerLink;
+      }
+      deferred.resolve(movie);
     }, function(error){
       deferred.reject(error);
     });
+    return deferred;
+  }
+
+  function netflixStatus(movie){
+    debugger;
+    var deferred = jQuery.Deferred();
+    var year = movie.release_date.split('-')[0];
+    var title = movie.original_title;
+    $.get('https://netflixroulette.net/api/api.php?title=' + title + '&year=' + year)
+    .done(function(data){
+      console.log(data);
+      movie.netflix = true;
+      deferred.resolve(movie);
+    }).fail(function(error){
+      console.log('That title is not currently available');
+      movie.netflix = false;
+      deferred.resolve(movie);
+  });
     return deferred;
   }
 
